7a92507f8dce2bfff5c7cb5ba7589a287d0efa33
hansog <hansog@outlook.com>
2018-07-10 09:43:24 +0200

    NVDB-3247:  Updated attributes in use on GeometryAttributes

--- src/main/java/no/vegvesen/nvdbapi/client/model/GeometryAttributes.java
+++ src/main/java/no/vegvesen/nvdbapi/client/model/GeometryAttributes.java
@@ -4,17 +4,17 @@ import java.time.LocalDate;
 
 public class GeometryAttributes {
 
-    private final LocalDate capturedDate;     //               Read by: roadnet & roadobject
-    private final LocalDate verifiedDate;     //
-    private final LocalDate updatedDate;      //
+    private final LocalDate capturedDate;     //               indexed by: roadnet
+    private final LocalDate verifiedDate;     //               indexed by: roadnet & roadobject
+    private final LocalDate updatedDate;      //               indexed by: roadnet
     private final String processHistory;      //
-    private final Integer municipality;       //               Read by: roadnet
-    private final String medium;             //               Read by: roadnet
+    private final Integer municipality;       //               indexed by: roadnet
+    private final String medium;              //               indexed by: roadnet
     private final Integer objectCode;         // (sosi name)
-    private final Integer classCode;          // (ltema)       Read by: roadnet
+    private final Integer classCode;          // (ltema)       indexed by: roadnet
     private final Boolean referenceGeometry;  //
-    private final Double length;              //               Read by: roadnet & roadobject
-    private final Integer heightRef;          //               Read by: roadnet
+    private final Double length;              //               indexed by: roadnet & roadobject
+    private final Integer heightRef;          //               indexed by: roadnet
 
     public GeometryAttributes(LocalDate capturedDate, LocalDate verifiedDate, LocalDate updatedDate, String processHistory, Integer municipality, String medium, Integer objectCode, Integer classCode, Boolean referenceGeometry, Double length, Integer heightRef) {
         this.capturedDate = capturedDate;
