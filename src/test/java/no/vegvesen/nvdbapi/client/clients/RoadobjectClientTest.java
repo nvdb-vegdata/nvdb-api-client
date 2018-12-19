750186b6bda4ef96ce3e1fdd17e120494514ea00
Marvin Bredal Lillehaug <marvin.lillehaug@gmail.com>
2018-12-19 10:06:02 +0100

    Test for downloading all features

---
+++ src/test/java/no/vegvesen/nvdbapi/client/clients/RoadobjectClientTest.java
@@ -0,0 +1,32 @@
+package no.vegvesen.nvdbapi.client.clients;
+
+import no.vegvesen.nvdbapi.client.util.Stopwatch;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class RoadobjectClientTest {
+
+    @Test
+    @Disabled("manual test")
+    void downloadAll() {
+        Stopwatch started = Stopwatch.createStarted();
+        ClientFactory clientFactory = new ClientFactory("https://nvdbw01.kantega.no/nvdb/api/v3",
+                "Jersey", "nvdbapi-client-test");
+        RoadObjectClient roadNetService = clientFactory.createRoadObjectClient();
+
+        RoadObjectClient.AsyncRoadObjectsResult result = roadNetService.getRoadObjectsAsync(
+                105,
+                RoadObjectRequest
+                        .newBuilder()
+                        .includeAll()
+                        .withAllVersions(true)
+                        .build());
+        result.get()
+                .toStream()
+                .forEach(element -> {
+                        System.out.println("Object: " + element.getId() + " " + element.getVersion());
+                });
+        Stopwatch stop = started.stop();
+        System.out.println(stop.elapsedMillis());
+    }
+}
