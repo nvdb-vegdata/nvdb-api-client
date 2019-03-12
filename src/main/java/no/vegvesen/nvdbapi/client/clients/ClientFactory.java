9a5892476f5d7d1f4037aa1e66744b230e818d07
Marvin Bredal Lillehaug <marvin.lillehaug@gmail.com>
2019-03-12 08:36:54 +0100

    config.connectorProvider(new ApacheConnectorProvider())

--- src/main/java/no/vegvesen/nvdbapi/client/clients/ClientFactory.java
+++ src/main/java/no/vegvesen/nvdbapi/client/clients/ClientFactory.java
@@ -32,6 +32,7 @@ import no.vegvesen.nvdbapi.client.model.datakatalog.Datakatalog;
 import no.vegvesen.nvdbapi.client.util.LoggingFilter;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.glassfish.jersey.apache.connector.ApacheClientProperties;
+import org.glassfish.jersey.apache.connector.ApacheConnectorProvider;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.ClientProperties;
 import org.glassfish.jersey.message.DeflateEncoder;
@@ -238,6 +239,8 @@ public final class ClientFactory implements AutoCloseable {
         if (debugLogger != null) {
             config.register(new LoggingFilter(debugLogger, true));
         }
+        config.connectorProvider(new ApacheConnectorProvider());
+
         config.property(ApacheClientProperties.CONNECTION_MANAGER, connectionManager);
         config.register(GsonMessageBodyHandler.class);
         config.register(
