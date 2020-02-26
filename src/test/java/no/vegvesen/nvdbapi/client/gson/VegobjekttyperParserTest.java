d1b09b9f01c810ea2a817b06877c67d1768d1940
Pål Thomassen <pal.thomassen@vegvesen.no>
2020-02-26 14:08:13 +0100

    Remove unused imports

--- src/test/java/no/vegvesen/nvdbapi/client/gson/VegobjekttyperParserTest.java
+++ src/test/java/no/vegvesen/nvdbapi/client/gson/VegobjekttyperParserTest.java
@@ -1,23 +1,33 @@
 package no.vegvesen.nvdbapi.client.gson;
 
-import no.vegvesen.nvdbapi.client.model.datakatalog.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
-
 import java.io.IOException;
 import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import no.vegvesen.nvdbapi.client.model.datakatalog.AttributeType;
+import no.vegvesen.nvdbapi.client.model.datakatalog.DataType;
+import no.vegvesen.nvdbapi.client.model.datakatalog.DoubleEnumAttributeType;
+import no.vegvesen.nvdbapi.client.model.datakatalog.EnumAttributeType;
+import no.vegvesen.nvdbapi.client.model.datakatalog.FeatureType;
+import no.vegvesen.nvdbapi.client.model.datakatalog.IntegerEnumAttributeType;
+import no.vegvesen.nvdbapi.client.model.datakatalog.StringEnumAttributeType;
+
 import static java.util.stream.Collectors.toMap;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
 import static no.vegvesen.nvdbapi.client.gson.Helper.parseObject;
 import static no.vegvesen.nvdbapi.client.gson.Helper.parsePlainList;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class VegobjekttyperParserTest {
     @ParameterizedTest
